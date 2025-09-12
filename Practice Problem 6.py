d217ccf8dbf9d5f63f36e6dca757f28d65cb7b50
HaganGrifin <109557015+HaganGrifin@users.noreply.github.com>
2025-09-12 13:54:45 -0500

    Delete Practice Problem 6.py

--- Practice Problem 6.py
+++
@@ -1,8 +0,0 @@
-range_start = int(input("From what number: "))
-range_end = int(input("To what number: "))
-
-for e in range(range_start, range_end + 1):
-    if e%2 == 0:
-        print(str(e) + " Is Even")
-    else:
-        print(str(e) + " Is Odd")
