d331536d3296f41e797862c16016a771ed28d1ec
HaganGrifin <109557015+HaganGrifin@users.noreply.github.com>
2025-09-12 13:55:02 -0500

    Delete Practice Problem 2.py

--- Practice Problem 2.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name? ")
-wages = int(input("What are your hourly wages? "))
-hours = int(input("How many hours did you work per day? "))
-days = int(input("How many days did you work? "))
-monthly_wages = wages*hours*days
-print("Your monthly wages are: " + str(monthly_wages))
